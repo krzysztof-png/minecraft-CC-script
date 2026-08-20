395679ed4e27067af5ac7d606f4697cef710b359
krzysztof-png <109978954+krzysztof-png@users.noreply.github.com>
2026-08-20 14:52:00 +0200

    Enhance diagnostic script for display devices

--- test.lua
+++ test.lua
@@ -1,35 +1,79 @@
--- Szukanie wyświetlacza (Display Link / Display Board)
-local dev = peripheral.find("create:display_link")
+--------------------------------------------------------------------------------
+--                SKRYPT DIAGNOSTYCZNO-TESTOWY (test.lua)                     --
+--------------------------------------------------------------------------------
+
+-- 1. Wykrywanie urządzenia
+local dev = peripheral.wrap("back")
+         or peripheral.find("create:display_link")
          or peripheral.find("create:display_board")
-         or peripheral.wrap("back")
+
+term.clear()
+term.setCursorPos(1, 1)
+print("=== DIAGNOSTYKA WYSWIETLACZA ===")
 
 if not dev then
-    error("Nie znaleziono wyswietlacza!")
+    error("Nie znaleziono peryferium na 'back' ani w sieci!")
 end
 
-local w, h = dev.getSize()
-
-local function pisz(linia, tekst)
-    if linia > h then return end
-    local formatowany = string.format("%-" .. w .. "s", tekst):sub(1, w)
-    
-    if dev.setLine then
-        dev.setLine(linia, formatowany)
-    elseif dev.setCursorPos and dev.write then
-        dev.setCursorPos(1, linia)
-        dev.write(formatowany)
+-- 2. Lista dostepnych metod peryferium
+print("\nDostepne metody w API:")
+local metody = {}
+for k, v in pairs(dev) do
+    if type(v) == "function" then
+        table.insert(metody, k)
     end
 end
+table.sort(metody)
+print(table.concat(metody, ", "))
 
--- Czyszczenie tablicy
-if dev.clear then dev.clear() end
+-- 3. Odczyt wymiarow
+if dev.getSize then
+    local w, h = dev.getSize()
+    print(string.format("\nWymiary: %d znakow x %d linii", w, h))
+end
 
--- Wypisanie testowej tablicy odjazdów
-local czas = textutils.formatTime(os.time(), true)
+-- 4. Proba czyszczenia
+print("\nCzyszczenie bufora...")
+if dev.clear then
+    pcall(dev.clear)
+end
 
-pisz(1, "TEST SYSTEMU [" .. czas .. "]")
-pisz(2, "1. BAZA GLOWNA   OK")
-pisz(3, "2. RAFINERIA   OPUZ")
-pisz(4, "3. KOPALNIA      --")
+-- 5. Proba zapisu roznymi metodami
+print("Zapisywanie tekstu testowego...")
 
-print(string.format("Wyslano test na wyswietlacz (%dx%d)", w, h))
+-- Metoda A: setCursorPos + write (standard Display Link / Terminal)
+if dev.setCursorPos and dev.write then
+    pcall(function()
+        dev.setCursorPos(1, 1)
+        dev.write("TEST CREATE 1")
+        dev.setCursorPos(1, 2)
+        dev.write("ODJAZD: 12:00")
+    end)
+end
+
+-- Metoda B: setLine (standard Display Board)
+if dev.setLine then
+    pcall(function()
+        dev.setLine(1, "TEST CREATE 1")
+        dev.setLine(2, "ODJAZD: 12:00")
+    end)
+end
+
+-- 6. Wymuszenie aktualizacji klap mechanicznych
+if dev.update then
+    local ok, err = pcall(dev.update)
+    if ok then
+        print("[OK] Wywolano dev.update()")
+    else
+        print("[!] Blad update(): " .. tostring(err))
+    end
+else
+    print("[i] Brak metody dev.update() - zapis natychmiastowy")
+end
+
+print("\n--------------------------------")
+print("Jesli tablica nadal nie reaguje:")
+print("1. Sprawdz czy Display Board ma obroty (RPM).")
+print("2. Kliknij Display Linkiem PPM na tablice,")
+print("   a potem zamontuj go z tylu komputera.")
+print("--------------------------------")
