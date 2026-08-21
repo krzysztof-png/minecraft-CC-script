f12f448f7a8a0d91072a209c0d01d40787ee73fe
krzysztof-png <109978954+krzysztof-png@users.noreply.github.com>
2026-08-21 19:03:47 +0200

    Add display test script for welcome and to-do messages

---
+++ display_test.lua
@@ -0,0 +1,18 @@
+display = peripheral.wrap("top")
+
+while true do
+display.clear()
+display.setCursorPos(1,1)
+display.write("Welcome Back!"
+display.setCursorPos(1,3)
+display.write("Home Sweat Home")
+display.update()
+sleep(5)
+display.clear()
+display.setCursorPos(1,1)
+display.write("To Do List")
+display.setCursorPos(1,3)
+display.write("- Fight the Ender Dragon")
+display.update()
+Sleep(5)
+end
