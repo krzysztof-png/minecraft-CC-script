3189a350aef805e0dc8e1e8170e7798618b3ecc5
krzysztof-png <109978954+krzysztof-png@users.noreply.github.com>
2026-08-21 15:34:41 +0200

    Refactor train signal testing script for better diagnostics

--- test_signal.lua
+++ test_signal.lua
@@ -1,36 +1,82 @@
--- test_signal.lua
--- Skrypt do testowania peryferii Train Signal
+-- test_bridge.lua
+-- Skrypt diagnostyczny dla CC:C Bridge
 
--- Znajdź sygnał w otoczeniu (zakładamy, że jest połączony przewodowo lub przylega bezpośrednio)
-local signal = peripheral.find("train_signal")
+local target = peripheral.find("train_signal") 
+    or peripheral.find("train_station") 
+    or peripheral.find("target")
 
-if not signal then
-    error("Nie znaleziono peryferii 'train_signal'. Sprawdź połączenie!")
+if not target then
+    print("Nie znaleziono peryferium Create/Bridge!")
+    print("Dostepne peryferia w sieci:")
+    for _, name in ipairs(peripheral.getNames()) do
+        print(" - " .. name .. " (" .. peripheral.getType(name) .. ")")
+    end
+    return
 end
 
-print("Test sygnału pociągu uruchomiony.")
-print("Obserwuję: " .. peripheral.getName(signal))
-print("---------------------------------------")
+local periName = peripheral.getName(target)
+local periType = peripheral.getType(target)
 
--- Funkcja pomocnicza do wyświetlania stanu
-local function printTrainStatus()
-    local trains = signal.listBlockingTrainNames()
-    if #trains > 0 then
-        print("Obecny pociąg: " .. trains[1])
-    else
-        print("Sygnał wolny (brak pociągu).")
+print("Polaczono z: " .. periName .. " [" .. periType .. "]")
+print("----------------------------------------")
+print("Dostepne metody:")
+for _, method in ipairs(peripheral.getMethods(periName)) do
+    print(" > " .. method)
+end
+print("----------------------------------------")
+
+-- Funkcja pomocnicza do odczytu danych w zaleznosci od dostepnych metod CC:C Bridge
+local function checkTrain()
+    print("\n[Odczyt danych " .. os.date("%T") .. "]")
+    
+    -- 1. Jezeli obiekt ma metode getTrainName (stacja / target)
+    if target.getTrainName then
+        local name = target.getTrainName()
+        print("getTrainName(): " .. tostring(name))
+    end
+
+    -- 2. Jezeli obiekt ma getTrain (obiekt tabeli z danymi pociagu)
+    if target.getTrain then
+        local trainData = target.getTrain()
+        if type(trainData) == "table" then
+            print("getTrain(): Dane pociagu:")
+            for k, v in pairs(trainData) do
+                print("   " .. tostring(k) .. " = " .. tostring(v))
+            end
+        else
+            print("getTrain(): " .. tostring(trainData))
+        end
+    end
+
+    -- 3. Jezeli obiekt to train_signal (listBlockingTrainNames / getBlockingTrains)
+    if target.listBlockingTrainNames then
+        local trains = target.listBlockingTrainNames()
+        print("listBlockingTrainNames(): Liczba pociagow: " .. #trains)
+        for i, tname in ipairs(trains) do
+            print("   " .. i .. ". " .. tname)
+        end
+    end
+
+    if target.getBlockingTrains then
+        local trains = target.getBlockingTrains()
+        print("getBlockingTrains(): Liczba wpisow: " .. (type(trains) == "table" and #trains or 0))
     end
 end
 
--- Początkowy odczyt
-printTrainStatus()
+-- Poczatkowy odczyt
+checkTrain()
 
--- Pętla nasłuchująca zdarzeń
+print("\nNasluchiwanie eventow (Ctrl+T aby przerwac)...")
 while true do
-    -- Czekaj na zdarzenie zmiany stanu sygnału
-    -- Create Mod wysyła 'train_signal_state_change'
-    local event, side = os.pullEvent("train_signal_state_change")
+    local eventData = { os.pullEvent() }
+    local eventName = eventData[1]
     
-    print("\n[ZMIANA STANU DETEKTORA]")
-    printTrainStatus()
+    -- Ignorujemy klikniecia myszka i odswiezanie ekranu
+    if eventName ~= "timer" and eventName ~= "mouse_click" and eventName ~= "mouse_up" then
+        print("\nOdebrano event: " .. eventName)
+        for i = 2, #eventData do
+            print("  arg[" .. (i-1) .. "]: " .. tostring(eventData[i]))
+        end
+        checkTrain()
+    end
 end
